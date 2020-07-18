e035055e614bbfac852ceb223037cd2d5182a901
Hanane Nogueira <hananenogueira@gmail.com>
2020-07-18 00:59:18 -0300

    Fizemos o metodo de lista

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 <?php 
 
 require_once("config.php");
+/*
+Carrega um usuário por ip
 
 $root = new Usuario();
 
 $root->loadbyId(1);
 
 echo $root;
+*/
 
+/* 
+Carrega toda a lista de usuarios 
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+/*
+Carrega o usuário pelo Login
+$search = Usuario::search("F");
+
+echo json_encode($search);
+*/
+
+/*
+Buscando na tabela por login e senha
+$usuario = new Usuario();
+$usuario->login("user","12345");
+
+echo $usuario;
+*/
  ?>
